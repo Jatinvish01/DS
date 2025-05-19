a0b4324112a45ae7e758d78dd1ec49c516449b8b
Jatin vishwakarma <vishwakarmajatin084@gmail.com>
2025-05-19 09:19:31 +0530

    Add files via upload

---
+++ filtering_data.py
@@ -0,0 +1,8 @@
+# Boolean Masking(Filtering Data) 
+
+import numpy as np
+
+arr = np.array([10,20,30,40,50,60,70])
+
+print(arr[arr > 20]) # array main jitte bhi 20 se bade element hai unhe print krega
+# [30,40,50,60,70]
